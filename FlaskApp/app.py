96e32d0faca72c42f267220fcaf4cdfb768306f6
Rukshar <Rukshar@MacBook-Pro-van-Rukshar.local>
2016-03-08 00:12:00 +0100

    Final application showing tweets on map including the message. TODO implement the input form

---
+++ FlaskApp/app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request
+import twitter_framework
+import random
+import string
+
+"""
+This is the server, first you have to run this code, then you can view the website
+by going to your browser and entering: localhost:5000
+Be aware that if you make changes, you first have to stop the server with CRTL+C and
+then run the server again, otherwise you get an error "address already in use" and then
+you have to quit Python.
+"""
+
+app = Flask(__name__)
+
+# Open the index.html to find the comment that has to be replaced with a script
+f = open('templates/index.html')
+index = f.read()
+f.close()
+exclude = set(string.punctuation)
+
+
+# Generate the script that should replace the comment in index.html
+def generate_marker_js(lat, lng, query, message):
+    # Every marker should have its unique id
+    rand = random.randint(0, 100000)
+    #Strip the messages from interpunction and line breaks (JS cannot handle most interpunctions)
+    query = ''.join(ch for ch in query if ch not in exclude)
+    message = ''.join(ch for ch in message if ch not in exclude)
+    query = query.replace('\n', ' ').replace('\r', '')
+    message = message.replace('\n', ' ').replace('\r', '')
+
+    return """ var contentString_%d = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h3 id="firstHeading" class="firstHeading"> %s </h3>'+
+            '<div id="bodyContent">'+
+            '<p> %s </p>'+
+            '</div>'+
+            '</div>';
+
+            var infowindow_%d = new google.maps.InfoWindow({
+            content: contentString_%d
+            });
+
+            var marker_%d = new google.maps.Marker({
+            position: {lat: %f, lng: %f},
+            map: map,
+            title: '%s'
+            });
+            marker_%d.addListener('click', function() {
+            infowindow_%d.open(map, marker_%d);
+            }); """ % (rand, query, message, rand, rand, rand, lat, lng, query, rand, rand, rand) 
+
+# Find the geo locations and messages from the tweets and send them to the client
+def place_markers(index, query='#SaySomethingGoodAboutTwitter'):
+    tweets = twitter_framework.extract_tweets(query)
+
+    marker_scripts_list = [generate_marker_js(tweet['location'][0], tweet['location'][1], query, tweet['message']) for tweet in tweets if tweet['location'] != None]
+    marker_script = '\n'.join(marker_scripts_list)
+
+    index = index.replace("// place_markers_here", marker_script)
+    return index
+
+index = place_markers(index)
+
+# Start server
+@app.route("/", methods=['POST', 'GET'])
+def main():
+    return index
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ FlaskApp/twitter_framework.py
@@ -0,0 +1,39 @@
+import twitter
+import json
+from geopy.geocoders import Nominatim
+
+consumer_key = 'OhCetqrIfnOpAklUngIPe9SRO'
+consumer_secret = 'AIN2M9MZg0egiXE5P5bJM2vsZPwp79hCjMgbnrTfvLvEQoEC9f'
+oauth_token = '695168737662603264-Cw5UkEVceQbh3cugKMIzprepaPtNMdg'
+oauth_secret = 'ZSHhT1lnwCkQDxwPr3hh64WXIDfZKMYOlfTxDEXkttwlx'
+auth = twitter.oauth.OAuth(oauth_token, oauth_secret, consumer_key, consumer_secret)
+twitter_api = twitter.Twitter(auth=auth)
+
+WORLD_WOE_ID = 1 # The Yahoo! Where On Earth ID for the entire world
+world_trends = twitter_api.trends.place(_id=WORLD_WOE_ID) # get back a callable
+#print world_trends
+
+def retrieve_tweets(q, count=100):
+	search_results = twitter_api.search.tweets(q=q, count=count)
+	statuses = search_results['statuses']
+
+	return statuses
+
+
+def get_geo_location(user_location):
+	geolocator = Nominatim()
+	try:
+		location = geolocator.geocode(user_location)
+		if location != None:
+			# print location.address.encode('utf-8')
+			return (location.latitude, location.longitude)
+	except:
+		return None
+
+
+def extract_tweets(query):
+	statuses = retrieve_tweets(query)
+	result = [{'location':get_geo_location(status['user']['location']), 
+			   'message':status['text']} for status in statuses]
+
+	return result
